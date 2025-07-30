ec86ec8135b7cb762aa42bf0742d1c8ce4492ae5
Albin Sunny <psalbin2005@gmail.com>
2025-07-30 22:19:15 +0530

    feat: Initial setup for Spring Boot backend and react native frontend

---
+++ vibe-navigator-api/vibe-navigator-api/src/main/java/com/vibenavigator/vibe_navigator_api/VibeNavigatorApiApplication.java
@@ -0,0 +1,13 @@
+package com.vibenavigator.vibe_navigator_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VibeNavigatorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VibeNavigatorApiApplication.class, args);
+	}
+
+}
---
+++ vibe-navigator-api/vibe-navigator-api/src/test/java/com/vibenavigator/vibe_navigator_api/VibeNavigatorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vibenavigator.vibe_navigator_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VibeNavigatorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
